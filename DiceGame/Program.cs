e082926c1eacbc6e984472b7b31cb846742a7d89
Aya-8 <dreamscometrue.h1821@gmail.com>
2025-07-03 14:18:11 +0900

    Greetings added

--- DiceGame/Program.cs
+++ DiceGame/Program.cs
@@ -10,6 +10,15 @@ public class DiceGame
         int dice_2 = random.Next(1, 7);
         int total = dice_1 + dice_2;
 
+        String name;
+
+        Console.WriteLine("What is your name ?");
+        Console.Write(">");
+        name = Console.ReadLine();
+
+        Console.WriteLine("Hello, " + name + "!");
+
+
         Console.WriteLine("Rolling dice...");
         Console.WriteLine("Die 1:" + dice_1);
         Console.WriteLine("Die 2:" + dice_2);
